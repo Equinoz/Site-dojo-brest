4b8488b64d23c8426cbe1f9d141f6f66a9c57468
Equinoz <julien.lecoz33@gmail.com>
2019-08-10 12:00:26 +0200

    Ajout d'un mini easter egg :)

---
+++ js/script.js
@@ -0,0 +1,4 @@
+let hidden_link = document.querySelector("header img");
+hidden_link.addEventListener("click", function() {
+	document.location.href = "https://christophe-lorreyte.fr/enso-cercle-japonais-invitation-a-meditation/";
+});
